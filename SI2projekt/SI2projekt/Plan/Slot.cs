be26534f6db072f3f3759a5507d201a3c781f689
artur <abuscitybus@gmail.com>
2017-05-28 19:14:43 +0200

    1. implentacja metod zapisywanie i odczytywanie plików tekstowych; 2. przechowywanie poszcegolnych skladnikow planu

---
+++ SI2projekt/SI2projekt/Plan/Slot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SI2projekt.Plan
+{
+    public class Slot//przechowywanie poszcegolnych skladnikow planu
+    {
+        public string Name;
+        public string Location;
+        public string LocationDetails;
+        public string LocationAdress;
+
+        public DateTime Start;
+        public DateTime Stop;
+        public TimeSpan Duration;
+    }
+}
