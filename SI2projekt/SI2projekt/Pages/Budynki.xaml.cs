e380f79d65b68c70b7a1b174cee0d98706fad8fe
sebastian <sebag93@gmail.com>
2017-05-29 10:26:23 +0200

    add buildings

---
+++ SI2projekt/SI2projekt/Pages/Budynki.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace SI2projekt.Pages
+{
+    public partial class Budynki : ContentPage
+    {
+        public Budynki()
+        {
+            InitializeComponent();
+        }
+    }
+}
